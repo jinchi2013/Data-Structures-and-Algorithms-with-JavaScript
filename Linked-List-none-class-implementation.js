bc8d59996e09b4c0829687a6b34cce96d1c65665
Calvin Jin <jinchi2013us@gmail.com>
2017-01-21 23:16:26 -0500

    Linked list basic operation with out class implementation

---
+++ Linked-List-none-class-implementation.js
@@ -0,0 +1,56 @@
+var list; // this would be a linked list
+
+// find function will not change the list
+// if the item exists in the list, the function will return the node
+// if not, this function will return null;
+function find(LinkedList, item) {
+	var currNode = LinkedList;
+	while (currNode.element != item && currNode !== null) {
+		currNode = currNode.next;
+	}
+	return currNode;
+}
+
+// insert function will change the LinkedList parameter
+function insert(LinkedList, newElement, item) {
+	var newNode = {
+		element: newElement,
+		next: null
+	};
+
+	var currNode = find(LinkedList, item);
+	// save currentnode next to the newnode next
+	newNode.next = currNode.next;
+	// connect currNode next with new Node
+	currNode.next = newNode;
+
+	// after this the global LinkedList will be changed.
+}
+
+// display function will show the element in the linked LinkedList one by one
+function display(LinkedList) {
+	var currNode = LinkedList;
+	while(currNode.next !== null) {
+		console.log(currNode.element);
+		currNode = currNode.next;
+	}
+	console.log(currNode.element);
+}
+
+// findPrevious node
+function findPrevious(LinkedList, item) {
+	var currNode = LinkedList;
+	while( currNode.next !== null && currNode.next.element !== item) {
+		currNode = currNode.next;
+	}
+	return currNode;
+}
+
+// remove function 
+// this function will also change the global list variable 
+function remove(LinkedList, item) {
+	var prevNode = findPrevious(item);
+	if(prevNode.next !== null) {
+		prevNode.next = prevNode.next.next;
+	}
+}
