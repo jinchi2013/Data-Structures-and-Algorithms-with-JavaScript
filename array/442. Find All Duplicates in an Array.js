c15c4e67c0ffbeff9fb1b669a06d7935007b9db1
Calvin Jin <jinchi2013us@gmail.com>
2017-02-15 17:27:06 -0500

    Two array practice

---
+++ array/442. Find All Duplicates in an Array.js
@@ -0,0 +1,58 @@
+/**
+ * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+ Find all the elements that appear twice in this array.
+
+ Could you do it without extra space and in O(n) runtime?
+
+ Example:
+ Input:
+ [4,3,2,7,8,2,3,1]
+
+ Output:
+ [2,3]
+ * */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    var result = [];
+    var obj = {};
+
+    nums.forEach(function(num){
+        if(obj.hasOwnProperty(num)) {
+            obj[num]++;
+        } else {
+            obj[num] = 0;
+        }
+    });
+
+    for(var prop in obj) {
+        if(obj[prop] === 1){
+            result.push(parseInt(prop));
+        }
+    }
+
+    return result;
+
+};
+
+var findDuplicate_quicker = function(nums) {
+    var length = nums.length;
+    var result = [];
+
+    for(var i = 0; i< length; i++) {
+        var index = Math.abs(nums[i]) - 1;
+
+        if(nums[index] < 0) {
+            result.push(nums[i]);
+        } else {
+            nums[index] = -nums[index];
+        }
+
+    }
+
+    return result;
+};
---
+++ array/448. Find All Numbers Disappeared in an Array.js
@@ -0,0 +1,42 @@
+/**
+ * Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+ Find all the elements of [1, n] inclusive that do not appear in this array.
+
+ Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+ Example:
+
+ Input:
+ [4,3,2,7,8,2,3,1]
+
+ Output:
+ [5,6]
+ * */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    var length = nums.length;
+    var result = [];
+
+    for(var i=0; i<length; i++) {
+        // -1 must be outside the Math.abs() method
+        // or the index will be missing 1
+        var val = Math.abs(nums[i]) - 1;
+
+        if(nums[val] > 0) {
+            nums[val] = -nums[val];
+        }
+    }
+
+    nums.forEach(function(num, index){
+        if(num > 0) {
+            result.push(index + 1);
+        }
+    });
+
+    return result;
+};
