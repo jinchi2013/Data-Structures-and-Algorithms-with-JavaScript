e043b7a3fa08a0a9592caca00c60ded4ece4b076
Calvin Jin <jinchi2013us@gmail.com>
2017-02-15 12:41:51 -0500

    two pointer solution

--- 142. Linked List Cycle II.js
+++ 142. Linked List Cycle II.js
@@ -5,6 +5,15 @@
 
  Note: Do not modify the linked list.
 
+ a cycle looks a like 1-2-3-4-2-3-4-2-3-4 ......continue and there is a loop in the list
+
+ this distance of slow pointer = a + b;
+ while the distance of fast pointer = a + b + c + b;
+ so get the conclusion 2(a+b) = a + b + c + b ==>> a = c
+ so if we have another two pointers each starts from head and the meeting point in the mid of cycle,
+ both move 1 step each time
+ they gonna meet at the cycle beginning node.
+
  Follow up:
  Can you solve it without using extra space?
  *
@@ -20,5 +29,32 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
+    var slow = head;
+    var fast = head;
+
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+
+        if(!fast.next) {
+            return null;
+        }
+
+        fast = fast.next;
+
+        if(fast === slow) {
+            var headStart = head;
+            var cycleMid = fast;
+
+            while(headStart !== cycleMid) {
+                headStart = headStart.next;
+                cycleMid = cycleMid.next;
+            }
+
+            return headStart;
+        }
+    }
+
+    return null;
 
 };
