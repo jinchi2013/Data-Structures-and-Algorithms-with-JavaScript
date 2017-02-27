792e91b1c647ddfd83cf56eab3d979ff8c4e23f5
Calvin Jin <jinchi2013us@gmail.com>
2017-02-27 14:53:35 -0500

    A basic hashTable class

---
+++ Hashing/HashTable.js
@@ -0,0 +1,34 @@
+function HashTable () {
+    var table = [];
+
+    // private method of hash table
+    var loseloseHashCode = function(key) {
+        var hash = 0;
+        for(var i = 0; i < key.length ; i++) {
+            hash += key.charCodeAt(i);
+        }
+
+        return hash % 37;
+    }
+
+    function put(key, value) {
+        var position = loseloseHashCode(key);
+        console.log(`${position}-${key}`);
+        table[position] = value;
+    }
+
+    function get(key) {
+        return table[loseloseHashCode(key)];
+    }
+
+    function remove(key) {
+        table[loseloseHashCode(key)] = undefined;
+    }
+
+    return {
+        get : get,
+        put : put,
+        remove : remove
+    }
+}
+
