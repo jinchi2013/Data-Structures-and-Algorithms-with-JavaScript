1d77e5e35ba30286abafc2f7a90465dfde31edfd
Calvin Jin <jinchi2013us@gmail.com>
2017-03-02 23:00:06 -0500

    nothing important

--- reverse digits of an integer.js
+++ reverse digits of an integer.js
@@ -12,7 +12,7 @@ If the integer's last digit is 0, what should the output be?
 ie, cases such as 10, 100. 
 Did you notice that the reversed integer might overflow? 
 Assume the input is a 32-bit integer, 
-then the reverse of 1000000003 overflows. 
+then the reverse of 1000000003 overflows.
 How should you handle such cases? 
  Throw an exception? 
  Good, but what if throwing an exception is not an option? 
