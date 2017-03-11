fc68f8ae8d9c1f27f6b79b275b7f0fd8c5e5a1ed
Calvin Jin <jinchi2013us@gmail.com>
2017-03-11 00:42:09 -0500

    Use DFS

---
+++ DFS/LetterCombinationsofaPhoneNumber.js
@@ -0,0 +1,59 @@
+// Given a digit string, return all possible letter combinations that the number could represent.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+
+
+// Input:Digit string "23"
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+// Note:
+// Although the above answer is in lexicographical order, your answer could be in any order you want.
+
+// Hide Company Tags Amazon Dropbox Google Uber Facebook
+// Hide Tags Backtracking String
+// Hide Similar Problems (M) Generate Parentheses (M) Combination Sum
+
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+// Take advantage of DFS
+var letterCombinations = function(digits) {
+	var numToLetters = {
+		'0': ' ',
+		'1': '',
+		'2': 'abc',
+		'3': 'def',
+		'4': 'ghi',
+		'5': 'jki',
+		'6': 'mno',
+		'7': 'pqrs',
+		'8': 'tuv',
+		'9': 'wxyz'
+	}
+
+	var res = [];
+
+	if(digits.length === 0) {
+		return res;
+	}
+
+	dfs(digits, i, current) {
+		if(i === digits.length) {
+			res.push(current);
+			return res;
+		}
+
+		var letters = numToLetters[digits[i]];
+
+		for(var i = 0; i < letters.length; i++) {
+			dfs(digits, i+1, current+letters[i]);
+		}
+	}
+
+	dfs(digits, 0, '');
+
+	return res;
+}
