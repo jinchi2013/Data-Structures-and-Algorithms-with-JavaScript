c8e9d39cb7d89328a79c3a523ae8ca0e18b990ed
Chi Jin <jinchi2013us@gmail.com>
2018-09-01 11:16:12 -0400

    add quick sort

--- quickSort/index.js
+++ quickSort/index.js
@@ -1,18 +1,18 @@
 function quicksort(arr){
   //base case
-  if(arr.length<=1){ return arr;}
+  if(arr.length<=1){ return arr}
   //now find swap position and value
   var swapPosition = Math.floor((arr.length-1)/2),
       swapValue = arr[swapPosition],
-      less = [], more = [];
-  arr = arr.slice(0, swapPosition).concat(arr.slice(swapPosition+1));
+      less = [], more = []
+  arr = arr.slice(0, swapPosition).concat(arr.slice(swapPosition+1))
 
-  for(var i=0;i<arr.length;i++){
+  for(var i=0; i<arr.length; i++){
     if(arr[i]< swapValue){
-      less.push(arr[i]);
+      less.push(arr[i])
     }else{
-      more.push(arr[i]);
+      more.push(arr[i])
     }
   }
-  return (quicksort(less)).concat([swapValue], quicksort(more));
+  return (quicksort(less)).concat([swapValue], quicksort(more))
 }
