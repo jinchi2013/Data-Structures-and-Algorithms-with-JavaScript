53a2304b02395e67630507c7d08dca4f270a252f
cjin <chi.jin@priceline.com>
2018-10-04 16:05:45 -0400

    add long c s

--- Dynamic-Programming/longest_common_substring.js
+++ Dynamic-Programming/longest_common_substring.js
@@ -6,6 +6,10 @@
   where DNA molecules are described using the first letter of the nucleobase of a nucleotid
 */
 
-function lcs(w1, w2) {
+function long_common_substring(w1, w2) {
+  let result = Array(w1.length).fill(
+    Array(w2.length).fill(0)
+  )
+
   
 }
