1b6549c2bb459acf758cae4f5586eb2816469ef3
cjin <chi.jin@priceline.com>
2018-10-08 08:08:33 -0400

    longest_common_substring add a map for it

--- Dynamic-Programming/longest_common_substring.js
+++ Dynamic-Programming/longest_common_substring.js
@@ -6,10 +6,39 @@
   where DNA molecules are described using the first letter of the nucleobase of a nucleotid
 */
 
-function long_common_substring(w1, w2) {
-  let result = Array(w1.length).fill(
-    Array(w2.length).fill(0)
-  )
+/*
+  a b c d e
+a 1 0 0 0 0
+s 0 0 0 0 0
+c 0 0 1 0 0
+d 0 0 0 2 0
+e 0 0 0 0 3
+f 0 0 0 0 0
+g 0 0 0 0 0
 
-  
+*/
+
+function longest_common_substring(w1, w2) {
+  let result = Array(w1.length)
+    .fill(0)
+    .forEach((row, index) => {
+      result[index] = Array(w2.length).fill(0)
+    })
+  let startIndex = null
+  let endIndex = null
+
+  for (let i = 0; i < w1.length; ++i) {
+    for(let j = 0; j < w2.length; ++j) {
+      if (w1[i] === w2[j]) {
+        
+        if (i === 0 || j === 0) {
+          result[i][j] = 1
+        } else {
+          result[i][j] = result[i-1][j-1] + 1
+        }
+      }
+    }
+  }
+
+  return result
 }
