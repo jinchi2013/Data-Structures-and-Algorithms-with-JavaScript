7d95c11762c1d2544bbc7427e521b01196aa1f04
cjin <chi.jin@priceline.com>
2018-10-25 09:49:31 -0400

    add comment

--- array/SmallestDifferenceBetweenTwoArrays/index.js
+++ array/SmallestDifferenceBetweenTwoArrays/index.js
@@ -1,16 +1,18 @@
 /*
 Smallest Difference pair of values between two unsorted Arrays
-Given two arrays of integers, compute the pair of values (one value in each array) with the smallest (non-negative) difference. Return the difference.
+Given two arrays of integers, 
+compute the pair of values (one value in each array) with the smallest (non-negative) difference. 
+Return the difference.
 
 Examples :
 
-Input : A[] = {1, 3, 15, 11, 2}
-        B[] = {23, 127, 235, 19, 8}
+Input : A[] = {1, 3, 15, 11, 2} =>     1  2  3  11  15
+        B[] = {23, 127, 235, 19, 8} => 8 19 23 127 235
 Output : 3
 That is, the pair (11, 8)
 
-Input : A[] = {l0, 5, 40}
-        B[] = {50, 90, 80}
+Input : A[] = {l0, 5, 40} => 5 10 40
+        B[] = {50, 90, 80} => 50 80 90
 Output : 10
 That is, the pair (40, 50)
 */
@@ -35,6 +37,10 @@ function smallestDifferenceBetween2Array(arr1, arr2) {
       result = diff
     }
 
+    // if we want to find the smallest diff
+    // we need to make two number more close to each other
+    // So if arr1[i] is larger, we increase arr2 by j++
+    // if arr2[j] is larger, we increase arr1 by i++
     if (arr1[i] < arr2[j]) {
       i++
     } else {
