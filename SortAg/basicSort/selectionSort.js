595d5505b694f87b846c5d96bc1967d62050807c
cjin <chi.jin@priceline.com>
2018-12-27 10:52:45 -0500

    search index in arry

--- SortAg/basicSort/selectionSort.js
+++ SortAg/basicSort/selectionSort.js
@@ -2,7 +2,7 @@ const { swap } = require('./../utils')
 
 function selectionSort (arr) {
   let min
-  for(let outer = 0; outer < arr.length - 1; ++outer) {
+  for(let outer = 0; outer < arr.length; ++outer) {
     min = outer
     for (let inner = outer + 1; inner < arr.length; ++inner ) {
       // compare the rest of the element with the store min
