f24ca7ad22e9d45b479092ddac6b2781f37255ad
Chi Jin <jinchi2013us@gmail.com>
2019-07-28 21:25:48 -0400

    update

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -1,39 +1,39 @@
 function Stack() {
-	let items = [];
-	return {
-		push: function(element) {
-			items.push(element);
-		},
-		pop: function() {
-			return items.pop();
-		},
-		peek: function() {
-			return items[items.length - 1];
-		},
-		isEmpty: function() {
-			return items.length === 0;
-		},
-		size: function () {
-			return items.length;
-		},
-		clear: function(){
-			items = [];
-		},
-		print: function() {
-			console.log(items.toString());
-		},
-		toString: function(){
-			return items.toString();
-		},
-		insertItems: function (i) {
-			if (i) {
-				items = [
-					...items,
-					...i
-				]
-			}
-		}
-	}
+  let items = [];
+  return {
+    push: function(element) {
+      items.push(element);
+    },
+    pop: function() {
+      return items.pop();
+    },
+    peek: function() {
+      return items[items.length - 1];
+    },
+    isEmpty: function() {
+      return items.length === 0;
+    },
+    size: function () {
+      return items.length;
+    },
+    clear: function(){
+      items = [];
+    },
+    print: function() {
+      console.log(items.toString());
+    },
+    toString: function(){
+      return items.toString();
+    },
+    insertItems: function (i) {
+      if (i) {
+        items = [
+          ...items,
+          ...i
+        ]
+      }
+    }
+  }
 }
 
 module.exports = Stack
