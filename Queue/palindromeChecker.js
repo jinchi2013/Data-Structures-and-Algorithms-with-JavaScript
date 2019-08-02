c5278818a10a7ac2bf3a896657b27beee3a17e8a
Chi Jin <jinchi2013us@gmail.com>
2019-08-01 23:20:32 -0400

    update palindromeChecker

--- Queue/palindromeChecker.js
+++ Queue/palindromeChecker.js
@@ -1,8 +1,9 @@
 const { Deque } = require('./Queue')
 
 function palindromeChecker (str) {
-  const strDq = Deque(Array.from(str))
+  const strDq = Deque(...Array.from(str.toLowerCase().replace(/[^a-zA-Z]/g, '')))
   while (!strDq.isEmpty()) {
+    if (strDq.peekFront())
     if (strDq.peekFront() === strDq.peekBack()) {
       if (strDq.size() === 1) {
         return true
@@ -18,4 +19,10 @@ function palindromeChecker (str) {
 }
 
 console.log(palindromeChecker('abba')) // true
-console.log(palindromeChecker('aba')) // true
+console.log(palindromeChecker('abca')) // false
+console.log('a', palindromeChecker('a'));
+console.log('aa', palindromeChecker('aa'));
+console.log('kayak', palindromeChecker('kayak'));
+console.log('level', palindromeChecker('level'));
+console.log('Was it a car or a cat I saw', palindromeChecker('Was it a car or a cat I saw'));
+console.log('Step on no pets', palindromeChecker('Step on no pets'));
