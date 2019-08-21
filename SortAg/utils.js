bf7ea31bd6577a2bbb4e64b7dd8c5e27f2cf90cb
cjin <chi.jin@priceline.com>
2019-08-21 15:13:51 -0400

    partition

--- SortAg/utils.js
+++ SortAg/utils.js
@@ -4,4 +4,28 @@ function swap(arr, index1, index2) {
   arr[index2] = temp;
 }
 
+function partition (arr, left, right) {
+  const median = Math.floor((left + right) / 2)
+  while (right >= left) {
+    while (arr[left] < arr[median]) {
+      ++left
+    }
+
+    while (arr[right] > arr[median]) {
+      --right
+    }
+
+    if (right > left) {
+      const temp = arr[right]
+      arr[right] = arr[left]
+      arr[left] = temp
+      ++left
+      --right
+    }
+  }
+
+  return left
+}
+
 exports.swap = swap
+exports.partition = partition
