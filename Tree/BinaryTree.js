653c2076855a4945f16737c2672f3b9558199806
cjin <chi.jin@priceline.com>
2019-10-15 16:34:34 -0400

    full TRAVERSAL

--- Tree/BinaryTree.js
+++ Tree/BinaryTree.js
@@ -26,4 +26,60 @@ BinaryTree.prototype.traversePreOrderIterative = function () {
   }
 }
 
+BinaryTree.prototype.traverseInOrder = function () {
+  traverseInOrderHelper(this._root)
+
+  function traverseInOrderHelper (node) {
+    if (!node) return
+
+    traverseInOrderHelper(node.left)
+    console.log(node.value)
+    traverseInOrderHelper(node.right)
+  }
+}
+
+BinaryTree.prototype.traverseInOrderIterative = function () {
+  var cur = this._root
+  var t = []
+  var done = false
+
+  while(!done) {
+    if (cur !== null) {
+      t.push(cur)
+      cur = cur.left
+    } else {
+      if (t.length) {
+        cur = t.pop()
+        console.log(cur.value)
+        cur = node.right
+      } else {
+        done = true
+      }
+    }
+  }
+}
+
+BinaryTree.prototype.traversePostOrder = function () {
+  traversePostOrderHelper(this._root)
+  function traversePostOrderHelper (node) {
+    if (node.left) traversePostOrderHelper(node.left)
+    if (node.right) traverseInOrderHelper(node.right)
+    console.log(node.value)
+  }
+}
+
+BinaryTree.prototype.traverseLevelOrder = function () {
+  var temp = this._root
+  var t = []
+  t.push(temp)
+
+  while (t.length) {
+    var node = t.shift()
+    console.log(node.value)
+
+    if (node.left) t.push(node.left)
+    if (node.right) t.push(node.right) 
+  }
+}
+
 exports.BinaryTree = BinaryTree
