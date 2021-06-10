0e1a444e6b9503ecdf5d0006a8a84f9da60391dd
Chi Jin <jinchi2013us@gmail.com>
2021-06-09 23:07:23 -0400

    Kahn's algorithiums
    Use incoming degree, adjacency list to do Topological sort

--- Graph/CourseSchedule-II-use-incoming_degree.js
+++ Graph/CourseSchedule-II-use-incoming_degree.js
@@ -48,34 +48,44 @@ var findOrder = function(numCourses, prerequisites) {
         to find the Topological Ordering of the courses
         
         create an array of incoming degree
+        create an adjacency list
         create a queue
-        handle points with same incoming degress by BFS
+
+        if any point x's incoming degree is 0, then it can be removed safely, 
+        so push that point into queue
+
+        1. use adjacency list for find the adjacent points for X
+        2. reduce all X's adjacent points' incoming degree, because x will be removed
+        3. after removed, if any x's adjacent point Y incoming degree is zero, push it to queue
+        4. repeat step 1 for Y
     */
     /*
         numCourses: 4
         prerequisites: [[1,0],[2,0],[3,1],[3,2]]
     */
-    // create adjacent list for the courses,
+    // create incoming degree array for each course
+    const incomingDegree = Array(numCourses).fill(0)
+    // create adjacent list for the courses
     const adjList = prerequisites.reduce((ls, prereq) => {
         const [crs, preCrs] = prereq
-        if (typeof ls[crs] !== 'undefined') {
-            ls[crs].push(preCrs)
+        if (typeof ls[preCrs] !== 'undefined') {
+            ls[preCrs].push(crs)
         } else {
-            ls[crs] = [preCrs]
+            ls[preCrs] = [crs]
         }
+        incomingDegree[crs]++
         return ls
-    }, [...Array(numCourses)])
-    // => [undefined,[0],[0],[1,2]], in fact this is not an adjacent list
-    // cause it say there is no point around 0, which is wrong,
-    // please see correct adjList in Graph/CourseSchedule-II-use-incoming_degree.js
-    
-    
+    }, [...Array(numCourses)].map(() => []))
+
+    // adjList: [[1,2], [3], [3]]
+    // incomingDegree: [0,1,1,2]
+
     // queue to always hande points with zero incoming degree
     var queue = []
     
     // push all courses with zero incoming degree into queue
-    for (let i = 0; i < adjList.length; ++i) {
-        if (!adjList[i]) {
+    for (let i = 0; i < incomingDegree.length; ++i) {
+        if (incomingDegree[i] === 0) {
             queue.push(i)
         }
     }
@@ -84,27 +94,27 @@ var findOrder = function(numCourses, prerequisites) {
         return []
     }
 
-    const result = []
+    const order = []
     let courseLeft = numCourses
 
     // As long as queue is not empty, we will need to look at adjList, the remove the courses in queue from adjList
     while (queue.length !== 0) {
         const curCrs = queue.shift() // get first course in queue
-        result.push(curCrs) // push course to result
-        --courseLeft // since 1 course is finished, reduce 1 from courseLeft
-        for (let j = 0; j < adjList.length; ++j) {
-            if (adjList[j] && adjList[j].includes(curCrs)) {
-                adjList[j] = adjList[j].filter(crs => crs !== curCrs) // remove current course from course j's prerequsites
-                if (adjList[j].length === 0) { // if course j's prerequsites is empty
-                    adjList[j] = undefined // mark it as undefined
-                    queue.push(j) // push course j to the queue
+        const reqCrses = adjList[curCrs] // get the All courses that rely on current course
+        if (reqCrses.length !== 0) { // if the requisites is not empty
+            for (let i = 0; c < reqCrses.length; ++i) {
+                --incomingDegree[reqCrses[i]] // reduce 1 incoming degree for required course
+                if (incomingDegree[reqCrses[i]] === 0) { // if there is no incoming degree
+                    queue.push(reqCrses[i]) // push this requisite course to the queue
                 }
             }
         }
+        order.push(curCrs) // push course to order
+        --courseLeft // since 1 course is finished, reduce 1 from courseLeft
     }
 
     if (courseLeft === 0) {
-        return result
+        return order
     }
 
     return []
